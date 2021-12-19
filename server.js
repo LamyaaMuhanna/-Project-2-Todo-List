85d7424fcf9d26da75001ba9e3290cb53c4749db
LamyaaMuhanna <lamiaoo2@hotmail.com>
2021-12-19 20:35:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
+const { error } = require('console')
 const express = require('express')
 const app = express()
 
 const db= require('./db')
 const Todo= require('./todo')
 
-app.get("/",(req,res)=>{
+app.get("/te",(req,res)=>{
     res.json("Get/ is Working")
 })
 app.get("/todos",(req,res)=>{
-    res.json("Get/ is Working..")
+    Todo.find({}, (err,data)=>{
+        if(err){
+            console.log("ERROR")
+        }else{
+            res.json(data)
+        }
+    })
 })
 
-
+app.post("/te",(req,res)=>{
+    Todo.create({}, (err, newTask)=>{
+        console.log('25', req.body)
+        if(err){
+            console.log("ERROR", err)
+        }else{
+            res.status(201).json(newTask)
+        }
+    })
+})
 
 app.listen(5000,() =>{
     console.log("SERVER IS WORKING")
