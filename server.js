676873e0a5bbf357929a50ba11349673d0d1a2a9
LamyaaMuhanna <lamiaoo2@hotmail.com>
2021-12-20 13:36:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const Todo= require('./todo')
 app.get("/te",(req,res)=>{
     res.json("Get/ is Working")
 })
+//to return all data 
 app.get("/todos",(req,res)=>{
     Todo.find({}, (err,data)=>{
         if(err){
@@ -20,13 +21,44 @@ app.get("/todos",(req,res)=>{
     })
 })
 
-app.post("/te",(req,res)=>{
+app.post("/todos",(req,res)=>{
     Todo.create({}, (err, newTask)=>{
-        console.log('25', req.body)
+        console.log('25:', req.body)
         if(err){
             console.log("ERROR", err)
         }else{
             res.status(201).json(newTask)
+        }
+    })
+})
+
+//delete by id
+app.delete("/todos/:id",(req,res)=>{
+    console.log('37', req.params.id)
+    Todo.deleteOne({_id: req.params.id}, (err, deleteObj)=>{
+        
+        if(err){
+            console.log("ERROR", err)
+        }else{
+            deleteObj.deleteCount===1
+           ? res.json("Delete this Task Successfully")
+           : res.status(404).json("this todos is not found")
+        }
+    })
+})
+
+//change the title
+app.put("/todos/:id",(req,res)=>{
+    Todo.updateOne(
+        {_id: req.params.id},
+        {title: req.body.newTitle},
+        (err, updateObj)=>{
+        if(err){
+            console.log("ERROR", err)
+        }else{
+            updateObj.modifiedCount===1
+           ? res.json("Update this Task Successfully")
+           : res.status(404).json("this todos is not found")
         }
     })
 })
