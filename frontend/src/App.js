48969c98dcb1bb09d880f47826bebc98a50543f0
LamyaaMuhanna <lamiaoo2@hotmail.com>
2021-12-22 16:58:01 +0300

    finish delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from "react";
-//import "./App.css";
+import "./App.css";
 import axios from 'axios'
 import Todos from "./components/Todos";
+import Add from "./components/Add"
 
 export default function App() {
   const [tasks, setTasks] = useState([])
@@ -9,7 +10,7 @@ export default function App() {
   useEffect(()=>{
     getData()
   },[])
-
+//to get data from server
   const getData= ()=>{
     axios
       .get('http://localhost:5000/todos')
@@ -20,17 +21,42 @@ export default function App() {
         console.log('ERR: ', err)
       })
   }
-  
-  
+  //to add new todo
+  const postNewTodo = (body)=>{
+    axios
+      .post('http://localhost:5000/todos',body)
+      .then((response)=>{
+        setTasks(response.data)
+        getData()
+      })
+      .catch((err)=>{
+        console.log('ERR: ', err)
+      })
+  }
+
+  //to delete Todo
+  const deleteTodo = (id)=>{
+    axios
+      .delete(`http://localhost:5000/todos/${id}`)
+      .then((response)=>{
+        setTasks(response.data)
+        getData()
+      })
+      .catch((err)=>{
+        console.log('ERR: ', err)
+      })
+  }
+
+
   const mapOverTasks = tasks.map((taskObj,i)=> <Todos
-  key={i} task={taskObj}/>)
+  key={i} task={taskObj} deleteTodo={deleteTodo}/>)
 
   return (
     <div className="App">
-      <button onClick={getData}>GET TASKS</button>
       {mapOverTasks}
-      
-      
+      <button onClick={getData}>GET TASKS</button>
+
+      <Add createFunc={postNewTodo} />
       
     </div>
   );
--- frontend/src/components/Todos.js
+++ frontend/src/components/Todos.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 
-
 export default function Todos(props) {
 
   const {_id,title,isCompleted}=props.task
   return (
     <div className="Todos">
-      <p>TITLE: {title}</p>
+    
+      <p style={{textDecoration:isCompleted?
+      'line-through':"none"}}>{title}</p>
+      <button  onClick={()=>{
+        props.deleteTodo(_id)
+      }}>x</button>
+      <input type="checkbox" checked={isCompleted} id=""/>
 
       
       
